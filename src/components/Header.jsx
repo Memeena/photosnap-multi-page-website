0deb139d2744e43c804f9ef94fa5caf39c70536a
Meenakshi <83496336+Memeena@users.noreply.github.com>
2024-11-19 16:09:06 +0530

    Logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,8 @@ import Logo from "./Logo";
 export default function Header({ width }) {
   return (
     <div>
-      <Logo />
+      <img src="./images/shared/desktop/logo.svg" alt="logo" />
+
       {width < 520 ? <MobileNav /> : <AppNav />}
     </div>
   );
