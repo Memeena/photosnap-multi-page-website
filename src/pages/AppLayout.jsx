9790d0ca35932d2240b8aa93e7df26136114581f
Meenakshi <83496336+Memeena@users.noreply.github.com>
2024-11-19 14:48:17 +0530

    commit

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,11 +1,9 @@
+import { Outlet } from "react-router-dom";
+
 export default function AppLayout() {
   return (
     <div>
-      <p
-        className="w-0 h-0 border-l-[50px] border-l-transparent
-  border-b-[75px] border-b-yellow-500
-  border-r-[50px] border-r-transparent "
-      ></p>
+      <Outlet />
     </div>
   );
 }
